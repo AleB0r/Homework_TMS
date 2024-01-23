18b0d3d50561a6100303331432c41adc984099e6
AleB0r <sasha2014993@gmail.com>
2024-01-23 15:14:21 +0300

    The function works with a string

--- Homework_9/task5.py
+++ Homework_9/task5.py
@@ -1,25 +1,24 @@
-def find_low_grades(file_name):
-    with open(file_name, 'r', encoding='utf-8') as file:
-        lines = file.readlines()
-
+def find_low_grades(lines):
     low_grades_students = []
+
     for line in lines:
-        # Разделение строки на фамилию, имя и оценку
         surname, name, grade = line.strip().split()
-        # Проверка оценки
         if int(grade) < 3:
-            # Добавление ученика с низкой оценкой в список
             low_grades_students.append(f"{surname} {name}")
 
     return low_grades_students
 
+if __name__ == "__main__":
+    file_name = "students.txt"
 
-file_name = "students.txt"
+    with open(file_name, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
 
-low_grades_students = find_low_grades(file_name)
-if low_grades_students:
-    print("Ученики с оценкой меньше 3:")
-    for student in low_grades_students:
-        print(student)
-else:
-    print("Нет учеников с оценкой меньше 3.")
+    low_grades_students = find_low_grades(lines)
+
+    if low_grades_students:
+        print("Ученики с оценкой меньше 3:")
+        for student in low_grades_students:
+            print(student)
+    else:
+        print("Нет учеников с оценкой меньше 3.")
