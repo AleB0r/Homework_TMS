14aa8a7fb944f206969d9b514004aa81844ef598
AleB0r <sasha2014993@gmail.com>
2024-02-01 20:14:16 +0300

    Add abstract class

--- Homework_13/task5.py
+++ Homework_13/task5.py
@@ -1,4 +1,8 @@
-class Strategy:
+from abc import ABC, abstractmethod
+
+
+class Strategy(ABC):
+    @abstractmethod
     def execute(self, num1, num2):
         raise NotImplementedError("Subclasses must implement execute method")
 
